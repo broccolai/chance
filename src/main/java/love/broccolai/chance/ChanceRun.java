c1845133f6f90278ef3c9c8c8b1d650d0d2625c3
cubitoverde <cubitverde@gmail.com>
2022-01-30 20:03:41 +0100

    Added multiple rolls simulation method

--- src/main/java/love/broccolai/chance/ChanceRun.java
+++ src/main/java/love/broccolai/chance/ChanceRun.java
@@ -1,6 +1,7 @@
 package love.broccolai.chance;
 
 import java.util.Collection;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 final class ChanceRun<R, C> {
@@ -40,6 +41,45 @@ final class ChanceRun<R, C> {
     private boolean roll(final Likelihood likelihood) {
         double rolledChance = ThreadLocalRandom.current().nextDouble();
         return likelihood.value() > rolledChance;
+    }
+
+    private int simulateRolls(final Likelihood likelihood, int rolls) {
+        // Simulate 'rolls' amount of times to obtain an object with a success chance of 'likelihood'.
+        // If possible, will normalize the distribution and obtain values therefrom.
+
+        double p = likelihood.value();
+        double mean = rolls * p;
+        double stddv = Math.sqrt(rolls * p * (1 - p));
+
+        boolean normalized = false;
+        double threeStddv;
+        if (p != 0) {
+            threeStddv = 9 * ((1 - p) / p);
+        } else {
+            threeStddv = 0;
+        }
+        if (rolls > threeStddv) {
+            normalized = true;
+        }
+
+        int amount;
+        if (normalized) {
+            Random random = new Random();
+            do {
+                amount = (int) (random.nextGaussian() * stddv + mean + 0.5);
+            } while (amount < 0 || amount > rolls);
+        } else {
+            // If it can not be normalized, do it the old fashioned way.
+
+            amount = 0;
+            for (int i = 0; i < rolls; i++) {
+                if (Math.random() < p) {
+                    amount++;
+                }
+            }
+        }
+
+        return amount;
     }
 
 }
